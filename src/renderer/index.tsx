31ba635ad8442c63d2ee41d089bcde965a18ac5d
luohuidong <luohuidong01@126.com>
2019-07-13 14:36:45 +0800

    将 React 组件应用到 DOM 上

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,2 +1,15 @@
 import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello world!</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
