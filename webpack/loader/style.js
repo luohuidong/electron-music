057de75d66e46232f7e6216c275c765c78c419ba
luohuidong <luohuidong01@126.com>
2019-07-14 14:04:10 +0800

    添加 TypeScript 定义文件，添加 mini-css-extract-plugin

---
+++ webpack/loader/style.js
@@ -0,0 +1,47 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// 判断是否为生产模式
+const prodMode = process.argv.NODE_ENV === 'production'
+
+const cssRegex = /\.css$/
+const cssModuleRegex = /\.module\.css$/
+
+const useStyleLoader = prodMode ? MiniCssExtractPlugin.loader : 'style-loader'
+
+const cssLoader = {
+  test: cssRegex,
+  exclude: cssModuleRegex,
+  use: [
+    useStyleLoader,
+    'css-loader'
+  ]
+}
+
+const cssModuleLoader = {
+  test: cssModuleRegex,
+  use: [
+    useStyleLoader,
+    {
+      loader: 'css-loader',
+      options: {
+        modules: true
+      }
+    }
+  ]
+}
+
+const sassLoader = {
+  test: /\.scss$/,
+  use: [
+    useStyleLoader,
+    {
+      loader: 'css-loader',
+      options: {
+        modules: true,
+      }
+    }, // translates CSS into CommonJS
+    'sass-loader' // compiles Sass to CSS, using Node Sass by default
+  ]
+}
+
+module.exports = [cssLoader, cssModuleLoader, sassLoader]
