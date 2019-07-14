fad9fa1d8aa793fddc4db046d9f0cd1c0d1e819e
luohuidong <luohuidong01@126.com>
2019-07-14 14:03:02 +0800

    首页添加样式

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
+import style from './style.scss'
+
 class App extends React.Component {
   render() {
     return (
-      <div>Hello world!</div>
+      <div className={style.container}>
+        <h1>
+          Hello world!
+        </h1>
+      </div>
     )
   }
 }
