c84f22cd8467f5caf241b02ab4f0a4d30932265e
luohuidong <luohuidong01@126.com>
2019-08-18 00:48:29 +0800

    封装精选歌单接口

--- src/renderer/api/http.ts
+++ src/renderer/api/http.ts
@@ -1,10 +1,31 @@
 import axios, { AxiosPromise } from 'axios'
 import { baseURL } from './config'
 
-export function get(url: string): AxiosPromise {
+interface GetParams {
+  url: string;
+  params?: object;
+  baseURL?: string;
+}
+
+/**
+ * axios get 封装
+ * @param options get 参数选项
+ */
+export function get(options: GetParams): AxiosPromise {
+  const { url, params } = options
+
   return axios({
+    baseURL,
     method: 'get',
     url,
-    baseURL,
+    params,
+  })
+}
+
+export function post(url: string, data: object): AxiosPromise {
+  return axios({
+    method: 'post',
+    url,
+    data
   })
 }
--- src/renderer/api/playList.ts
+++ src/renderer/api/playList.ts
@@ -1,1 +1,38 @@
-function request
+import { get } from './http'
+
+interface HighQualityPlayList {
+  name: string;
+  id: number;
+  description: string;
+  coverImgUrl: string;
+}
+
+interface HighQualityPlayListReturnData {
+  playlists: HighQualityPlayList[];
+  total?: number;
+  more?: boolean;
+}
+
+export async function requestHighQualityPlayList(): Promise<HighQualityPlayListReturnData> {
+  try {
+    const config = {
+      url: '/top/playlist/highquality',
+    }
+    const { data } = await get(config)
+    const { playlists, code, total, more } = data
+
+    if (code !== 200) {
+      throw new Error('获取列表失败')
+    }
+
+    return {
+      playlists,
+      total,
+      more
+    }
+  } catch (error) {
+    return {
+      playlists: []
+    }
+  }
+}
