3c652a6d531fea4dc9ac7d98d6d086e627da2f19
luohuidong <luohuidong01@126.com>
2019-08-18 00:49:58 +0800

    playlist reducer import  别名更改；导出全局 state type，用于 typescript 类型校验。

--- src/renderer/store/index.ts
+++ src/renderer/store/index.ts
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 
 
-import { reducer as PlayList } from './pages/PlayList'
+import { reducer as playListReducer } from './pages/PlayList'
 
 const rootReducer = combineReducers({
-  PlayList
+  playList: playListReducer
 })
+
+export type AppState = ReturnType<typeof rootReducer>
 
 export const store = createStore(
   rootReducer,
