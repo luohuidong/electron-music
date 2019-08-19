7ac7c440dce6ac9812df80b7bb0618bea11c8f26
luohuidong <luohuidong01@126.com>
2019-08-19 22:54:15 +0800

    新增歌单封面组件

---
+++ src/renderer/components/Cover/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './style.scss'
+
+function Cover(): React.ReactElement {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Cover
