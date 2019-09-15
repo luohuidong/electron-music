a7ae7e3755a99f06e620f80021f66b867de97b2f
luohuidong <luohuidong01@126.com>
2019-09-15 23:40:02 +0800

    修复 loader include 路径不正确的问题

--- webpack/renderer-loader/file.js
+++ webpack/renderer-loader/file.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   test: /\.(png|svg|jpg|gif)$/,
-  include: path.resolve(__dirname, 'src'),
+  include: path.resolve(__dirname, '../../src'),
   use: [
     'file-loader'
   ]
