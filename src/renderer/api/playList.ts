e4e1b92b9bd87260a1fe18459a5d4db7b645f1d6
luohuidong <luohuidong01@126.com>
2019-09-17 08:31:06 +0800

    新增歌单详情接口封装

--- src/renderer/api/playList.ts
+++ src/renderer/api/playList.ts
@@ -39,3 +39,48 @@ export async function requestHighQualityPlayList(): Promise<HighQualityPlayListR
     }
   }
 }
+
+/** 歌单歌曲数据 */
+interface Songs {
+  name: string;
+  id: number;
+}
+
+/** 歌单歌曲 id */
+interface SongIds {
+  id: number;
+}
+
+/** 歌单详情数据 */
+interface PlayListDetailData {
+  tracks: Songs[];
+  trackIds: SongIds[];
+  id: number; // 歌单 id
+  tags: string[]; // 标签
+}
+
+/**
+ * 获取歌单详情
+ * @param playListId 歌单 id
+ */
+export async function requestPlaylistDetail(playListId: string): Promise<PlayListDetailData> {
+  try {
+    const config = {
+      url: '/playlist/detail',
+      params: {
+        id: playListId
+      }
+    }
+
+    const { data } = await get(config)
+    const { code, playlist } = data
+
+    if (code !== 200) {
+      throw new Error('获取歌单详情失败')
+    }
+
+    return playlist
+  } catch (error) {
+    throw new Error('获取歌单详情失败')
+  }
+}
