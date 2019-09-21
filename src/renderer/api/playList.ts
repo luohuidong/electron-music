4e58cf86c6a5d294ca08946facf9fe91a54b0c4a
luohuidong <luohuidong01@126.com>
2019-09-21 23:57:25 +0800

    Encapsulate request song's play url logic.

--- src/renderer/api/playList.ts
+++ src/renderer/api/playList.ts
@@ -84,3 +84,35 @@ export async function requestPlaylistDetail(playListId: number): Promise<PlayLis
     throw new Error('获取歌单详情失败')
   }
 }
+
+
+
+interface SongPlayUrlData {
+  id: string;
+  url: string;
+}
+
+/**
+ * 获取多首歌的播放地址
+ * @param songIds 当有多首歌的时候，每首歌的 id 通过 ',' 隔开
+ */
+export async function requestSongPlayUrls(songIds: string): Promise<SongPlayUrlData[]> {
+  try {
+    const config = {
+      url: '/song/url',
+      params: {
+        id: songIds
+      }
+    }
+
+    const { data } = await get(config)
+    
+    if (data.code !== 200) {
+      throw new Error('获取歌曲播放地址失败')
+    }
+
+    return data.data
+  } catch (error) {
+    throw new Error('消息提醒')
+  }
+}
