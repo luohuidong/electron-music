a102960638808ba8609951f50b2a3f7b77f1ee30
luohuidong <luohuidong01@126.com>
2019-09-28 19:58:16 +0800

    添加获取歌曲播放 url 跟播放进度百分比的逻辑

--- src/renderer/components/Player/Audio/index.tsx
+++ src/renderer/components/Player/Audio/index.tsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
 
-function Audio(): JSX.Element {
+import { requestSongPlayUrls } from 'Api/playList'
+import { AppState } from 'Store/index'
+import { types as playerTypes, actions as playerActions } from 'Store/components/Player'
+
+interface Props {
+  currentSong: playerTypes.Song;
+  savePercentage: typeof playerActions.savePercentage;
+}
+
+type EffectCallBackReturnType = void | (() => void)
+
+function Audio(props: Props): JSX.Element {
+  let audioRef = React.createRef<HTMLAudioElement>()
+
+  // 获取当前播放可取的播放 url
+  const [playUrl, setPlayUrl] = useState()
+  useEffect((): void => {
+    const { currentSong } = props
+
+    if (currentSong.id) {
+      requestSongPlayUrls(`${currentSong.id}`).then((songPlayUrlDatas): void => {
+        const songPlayUrlData = songPlayUrlDatas[0]
+        const playUrl = songPlayUrlData.url
+        setPlayUrl(playUrl)
+      })
+    }
+  }, [props.currentSong])
+
+  // 获取当前播放进度百分比
+  useEffect((): EffectCallBackReturnType => {
+    function handleTimeUpdate(e): void {
+      const { target } = e
+      const { currentTime, duration } = target
+
+      if (!currentTime || !duration) {
+        return
+      }
+
+      const percentage = currentTime / duration
+      props.savePercentage(percentage)
+    }
+
+    const audio = audioRef.current
+
+    if (audio) {
+      audio.addEventListener('timeupdate', handleTimeUpdate)
+    }
+
+    return (): void => {
+      if (audio) {
+        audio.removeEventListener('timeupdate', handleTimeUpdate)
+      }
+    }
+  }, [])
+
   return (
-    <audio>
-      
-    </audio>
+    <div>
+      <audio src={playUrl} controls ref={audioRef} autoPlay></audio>
+    </div>
   )
 }
 
-export default Audio
+interface State {
+  currentSong: playerTypes.Song;
+}
+
+const mapStateToProps = ({ player }: AppState): State => ({
+  currentSong: player.currentSong,
+})
+
+const mapDispatchToProps = {
+  savePercentage: playerActions.savePercentage
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Audio)
