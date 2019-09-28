8127c15f4ffdb4423bb86795f6464c9098866cd6
luohuidong <luohuidong01@126.com>
2019-09-28 16:11:44 +0800

    开发模式下才打开调试面板

--- src/main/createWindow.js
+++ src/main/createWindow.js
@@ -3,7 +3,7 @@ const { BrowserWindow } = require('electron')
 const mode = process.env.NODE_ENV
 const isDevMode = mode === 'development'
 
-function createWindow (win) {
+function createWindow(win) {
   // Create the browser window.
   win = new BrowserWindow({
     width: 1500,
@@ -15,14 +15,15 @@ function createWindow (win) {
 
   if (isDevMode) {
     win.loadURL('http://localhost:8080/')
+
+    // Open the DevTools.
+    win.webContents.openDevTools()
   } else {
     // and load the index.html of the app.
     // filePath should be a path to an HTML file relative to the root of your application
     win.loadFile('dist-webpack/renderer/index.html')
   }
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
