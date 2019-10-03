9ddda9cb7d206bc1eadeaf499198b9b693469789
luohuidong <luohuidong01@126.com>
2019-10-03 19:05:22 +0800

    添加播放器播放和暂停音乐的逻辑

--- src/renderer/components/Player/Button/Play/index.tsx
+++ src/renderer/components/Player/Button/Play/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { AppState } from 'Store/index'
+import { actions as playerActions } from 'Store/components/Player/index'
 
 import playIcon from './play.svg'
-import pauseIcon from './play.svg'
+import pauseIcon from './pause.svg'
 import styles from './style.scss'
 
 function Play(): JSX.Element {
+  const dispatch = useDispatch()
+
+  const playState = useSelector(({ player }: AppState): boolean => player.playState)
+  const videoElement = useSelector(({ player }: AppState): HTMLAudioElement | undefined => player.audioElement)
+
+  function handleClick(): void {
+    if (!videoElement) {
+      return
+    }
+
+    if (playState) {
+      videoElement.pause()
+      dispatch(playerActions.savePlayState(false))
+    } else {
+      videoElement.play()
+      dispatch(playerActions.savePlayState(true))
+    }
+  }
+
+
   return (
-    <img src={playIcon} className={styles.img} alt="播放" title="播放" />
+    <img
+      src={playState ? pauseIcon : playIcon}
+      className={styles.img}
+      alt={playState ? '暂停' : '播放'}
+      title={playState ? '暂停' : '播放'}
+      onClick={handleClick}
+    />
   )
 }
 
