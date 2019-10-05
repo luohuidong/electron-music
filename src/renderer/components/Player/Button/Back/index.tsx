a05c9de8d10a848e0b3854f6d4353af2bd65830a
luohuidong <luohuidong01@126.com>
2019-10-05 09:21:02 +0800

    Add change song logic

--- src/renderer/components/Player/Button/Back/index.tsx
+++ src/renderer/components/Player/Button/Back/index.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useEffect, MouseEvent } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { AppState } from 'Store/index'
+import { EffectCallBack } from 'Types/index'
+import { types as playerTypes, actions as playerActions } from 'Store/components/Player'
+
 
 import backIcon from './back.svg'
 import styles from './style.scss'
 
 function Back(): JSX.Element {
+  const dispatch = useDispatch()
+  const playerState = useSelector(({ player }: AppState): playerTypes.State => player)
+
+  function handleClick(e: MouseEvent): void {
+    e.stopPropagation()
+
+    const playList = playerState.playList
+    const length = playList.length
+
+    /** 前一首歌在歌曲列表中的索引 */
+    let previousSongIndex = playerState.currentSongIndex + 1
+
+    if (previousSongIndex < 0) {
+      previousSongIndex = length - 1
+    }
+
+    dispatch(playerActions.saveCurrentSongIndex(previousSongIndex))
+    dispatch(playerActions.saveCurrentSong(playList[previousSongIndex]))
+  }
+
   return (
-    <img 
-      className={styles.img} 
-      src={backIcon} 
-      alt="上一首歌" 
-      title="上一首歌" 
+    <img
+      className={styles.img}
+      src={backIcon}
+      alt="上一首歌"
+      title="上一首歌"
+      onClick={handleClick}
     />
   )
 }
--- src/renderer/components/Player/Button/Next/index.tsx
+++ src/renderer/components/Player/Button/Next/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { AppState } from 'Store/index'
 
 import backIcon from './next.svg'
 import styles from './style.scss'
 
 function Next(): JSX.Element {
+  const dispatch = useDispatch()
+  const playerState = useSelector(({ player }: AppState): playerTypes.State => player)
+
+  function handleClick(e: MouseEvent): void {
+    e.stopPropagation()
+
+    const playList = playerState.playList
+    const length = playList.length
+
+    /** 前一首歌在歌曲列表中的索引 */
+    let previousSongIndex = playerState.currentSongIndex + 1
+
+    if (length <= previousSongIndex) {
+      previousSongIndex = 0
+    }
+
+    dispatch(playerActions.saveCurrentSongIndex(previousSongIndex))
+    dispatch(playerActions.saveCurrentSong(playList[previousSongIndex]))
+  }
+
   return (
-    <img 
-      className={styles.img} 
-      src={backIcon} 
-      alt="下一首歌" 
-      title="下一首歌" 
+    <img
+      className={styles.img}
+      src={backIcon}
+      alt="下一首歌"
+      title="下一首歌"
     />
   )
 }
