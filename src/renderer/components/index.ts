ade51d73bd3d68ad40dda35c3f84b13bc1a3ae9b
luohuidong <luohuidong01@126.com>
2019-10-06 22:24:42 +0800

    Export scroll view component

--- src/renderer/components/index.ts
+++ src/renderer/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Cover } from './Cover'
 export { default as Layout } from './Layout'
-export { default as Player} from './Player'
+export { default as Player } from './Player'
+export { default as ScrollView } from './ScrollView'
