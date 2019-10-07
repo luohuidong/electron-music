38465484acf7a8cb4c49278b31bdfc083edd2b2b
luohuidong <luohuidong01@126.com>
2019-10-07 22:18:05 +0800

    Add Slider component

---
+++ src/renderer/components/Slider/index.tsx
@@ -0,0 +1,19 @@
+import React, { MouseEvent } from 'react'
+
+import styles from './style.scss'
+
+export default function Slider(): JSX.Element {
+  function handleClick(e: MouseEvent): void {
+    e.stopPropagation()
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.bar}>
+        <div className={styles.button} onClick={handleClick}>
+          <div className={styles.centerCircle} />
+        </div>
+      </div>
+    </div>
+  )
+}
