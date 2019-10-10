63ebe910d64ab99b652300cdf77e254352aa306f
luohuidong <luohuidong01@126.com>
2019-10-10 09:16:23 +0800

    添加获取 scroll view 的滚动百分比

--- src/renderer/components/ScrollView/index.tsx
+++ src/renderer/components/ScrollView/index.tsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classnames from 'classnames'
 
+import { EffectCallBack } from 'Types/index'
 import styles from './style.scss'
 
 interface Props {
   className?: string;
   children: JSX.Element;
+  handleScroll?: Function;
 }
 
 export default function ScrollView(props: Props): JSX.Element {
   const { className } = props
+  const containerRef = React.createRef<HTMLDivElement>()
+
+  useEffect((): EffectCallBack => {
+    const containerElement = containerRef.current
+
+    function handleScroll(event): void {
+      const { target } = event
+      const { scrollHeight, scrollTop, clientHeight } = target
+
+      const percentage = (scrollTop + clientHeight) / scrollHeight
+      props.handleScroll && props.handleScroll(percentage)
+    }
+
+    if (containerElement) {
+      containerElement.addEventListener('scroll', handleScroll)
+    }
+
+    return (): void => {
+      if (containerElement) {
+        containerElement.removeEventListener('scroll', handleScroll)
+      }
+    }
+  }, [])
 
   return (
-    <div className={classnames(styles.container, className)}>
+    <div
+      ref={containerRef}
+      className={classnames(styles.container, className)}
+    >
       {props.children}
     </div>
   )
