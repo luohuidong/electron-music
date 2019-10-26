98b3a7b5829ddbaf13623e5b379bf19045e2896d
luohuidong <luohuidong01@126.com>
2019-10-26 19:37:20 +0800

    Modify the requestPlayList function parameter

--- src/renderer/api/playList/requestPlayList.ts
+++ src/renderer/api/playList/requestPlayList.ts
@@ -1,5 +1,12 @@
 import { get } from '../http'
 
+interface PlayListQueryParams {
+  order?: string; // 'new' or 'hot'，分别对应这最新歌单和最热歌单
+  category?: string; // 歌单分类，如 "华语", "古风", "欧美" 等，可从歌单分类接口获取(/playlist/catlist) 中获取
+  limit?: number; // 结果返回数量，如果不传默认为 50
+  offset?: number; // 偏移量，用于分页，如果不传默认为 0
+}
+
 interface PlayList {
   name: string;
   id: number;
@@ -15,18 +22,23 @@ interface PlayListReturnData {
 
 /**
  * 获取精品歌单
- * @param order 'new' or 'hot'，分别对应这最新歌单和最热歌单
- * @param category 如 "华语", "古风", "欧美" 等，可从歌单分类接口获取(/playlist/catlist) 中获取
+ * @param order
+ * @param category
  */
-export async function requestPlayList(order = 'hot', category = '全部'): Promise<PlayListReturnData> {
+export async function requestPlayList(params: PlayListQueryParams): Promise<PlayListReturnData> {
+  const { order = 'hot', category = '全部歌单', limit = 50, offset = 0 } = params
+
   try {
     const config = {
       url: '/top/playlist',
       params: {
         order,
-        category
+        cat: category,
+        limit,
+        offset,
       }
     }
+
     const { data } = await get(config)
     const { playlists, code, total, more } = data
 
