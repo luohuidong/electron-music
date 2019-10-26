303454cd8626ec7583c3c2217cf702dd0ca12b33
luohuidong <luohuidong01@126.com>
2019-10-26 21:23:12 +0800

    Add the case of saving query params.

--- src/renderer/store/pages/PlayList/reducers.ts
+++ src/renderer/store/pages/PlayList/reducers.ts
@@ -1,9 +1,10 @@
-import { State, SAVE_PLAY_LIST, ActionTypes } from './types'
+import { State, ActionTypes, SAVE_PLAY_LIST, SAVE_PLAY_LIST_QUERY_PARAMS } from './types'
 import { state as initialState } from './state'
 
 export default function(state = initialState, action: ActionTypes): State  {
   switch (action.type) {
     case SAVE_PLAY_LIST:
+    case SAVE_PLAY_LIST_QUERY_PARAMS:
       return {
         ...state,
         ...action.payload
