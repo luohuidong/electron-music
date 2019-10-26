c57db677ac911ca631a7131efc18addeaf6e459c
luohuidong <luohuidong01@126.com>
2019-10-26 20:41:12 +0800

    Refresh the playlist data after choosing the category.

--- src/renderer/pages/PlayList/Category/ListItem.tsx
+++ src/renderer/pages/PlayList/Category/ListItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import { useDispatch } from 'react-redux'
 import { CategoryListItem } from 'Api/playList/requestCategoryList'
+import { thunks as playListThunks } from 'Store/pages/PlayList'
 
 import styles from './listItem.scss'
 
@@ -10,9 +11,20 @@ interface Props {
 
 export default function ListItem(props: Props): JSX.Element {
   const { data } = props
+  const dispatch = useDispatch()
+
+  function handleClick(): void {
+    const params = {
+      order: 'hot',
+      category: data.name,
+      limit: 50,
+      offset: 0
+    }
+    dispatch(playListThunks.thunkSavePlayList(params))
+  }
 
   return (
-    <a className={styles.link}>
+    <a className={styles.link} onClick={handleClick}>
       {data.name}
     </a>
   )
