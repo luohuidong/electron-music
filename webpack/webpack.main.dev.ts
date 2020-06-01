06e724a52fcab32c2a8e47b98f7a290435d8de6a
luohuidong <luohuidong01@126.com>
2020-06-02 00:41:29 +0800

    新增main process development模式的webpack配置

--- webpack/webpack.main.dev.ts
+++ webpack/webpack.main.dev.ts
@@ -3,6 +3,6 @@ import merge from 'webpack-merge';
 import common from './webpack.main.common'
 
 export default () => merge(common(), {
-  mode: 'production',
+  mode: 'development',
   target: 'electron-main',
 });
