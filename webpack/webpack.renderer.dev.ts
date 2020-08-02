a0d3f2c7b1af3b1558b2c1f36d795d1147ebce1f
luohuidong <luohuidong01@126.com>
2020-08-02 17:56:54 +0800

    更新 merge 函数引入方式

--- webpack/webpack.renderer.dev.ts
+++ webpack/webpack.renderer.dev.ts
@@ -1,16 +1,17 @@
-import path from 'path';
-import merge from 'webpack-merge';
+import path from "path";
+import { merge } from "webpack-merge";
 
-import { Env } from './type';
-import common from './webpack.renderer.common';
+import { Env } from "./type";
+import common from "./webpack.renderer.common";
 
-export default (env: Env) => merge(common(env), {
-  mode: 'development',
-  target: "electron-renderer",
-  devtool: 'inline-source-map',
-  devServer: {
-    // This tells webpack-dev-server to serve the files from the dist/webpack directory on localhost:8080.
-    contentBase: path.join(__dirname, '../dist/renderer'),
-    hot: true,
-  },
-});
+export default (env: Env) =>
+  merge(common(env), {
+    mode: "development",
+    target: "electron-renderer",
+    devtool: "inline-source-map",
+    devServer: {
+      // This tells webpack-dev-server to serve the files from the dist/webpack directory on localhost:8080.
+      contentBase: path.join(__dirname, "../dist/renderer"),
+      hot: true,
+    },
+  });
--- webpack/webpack.renderer.prod.ts
+++ webpack/webpack.renderer.prod.ts
@@ -1,4 +1,4 @@
-import merge from 'webpack-merge';
+import { merge } from 'webpack-merge';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 import { Env } from './type';
